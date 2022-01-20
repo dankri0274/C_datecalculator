491c8919ba4f8d77a82788cb43818992f09cc868
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2022-01-20 11:36:00 +0100

    Added main file

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void calc(int days) {
+	int years = days / 365;
+	int months = (days % 365) / 30;
+	int days_left = (days % 365) % 30;
+
+	printf("\n%d days is:\n%d years\n%d months\n%d days\n\n", days, years, months, days_left);
+}
+
+int main(int argc, char *argv[]) {
+	calc(atoi(argv[1]));
+}
